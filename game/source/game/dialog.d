d21de266263161ca4adfbd7055a5aa4733d1b5ce
Unknown <enalyen@gmail.com>
2019-01-12 01:59:18 +0100

    Better bubble

--- game/source/game/dialog.d
+++ game/source/game/dialog.d
@@ -6,8 +6,13 @@ DialogGui dialogGui;
 
 class DialogGui: GuiElement {
     private {
+        NinePatch _bubble;
         Text _name;
         Text _msg;
+        Vec2f _originalSize = Vec2f.one, _newSize = Vec2f.one;
+        Timer _timer, _timer2;
+        int _characterId;
+        string _newMsg;
     }
 
     this() {
@@ -24,11 +29,46 @@ class DialogGui: GuiElement {
 
         _msg = new Text("");
         box.addChildGui(_msg);
+        _bubble = fetch!NinePatch("bubble");
+        _bubble.size = size;
     }
 
     void setNewDialog(string name, string msg) {
         _name.text = name;
-        writeln(_name.text);
         _msg.text = msg;
+        _originalSize = _bubble.size;
+        _newSize = _msg.size + Vec2f(80f, 80f);
+        _msg.text = " ";
+        _msg.text = "";
+        _timer.start(1f);
+        _timer2.start(0.02f);
+        _newMsg = msg;
+        _characterId = 0;
+    }
+
+    override void update(float deltaTime) {
+        _timer.update(deltaTime);
+        _timer2.update(deltaTime);
+        _bubble.size = _originalSize.lerp(_newSize, easeOutBounce(_timer.time));
+
+        if(_characterId < _newMsg.length && !_timer2.isRunning) {
+            if(_newMsg[_characterId] == '{') {
+                while(_newMsg[_characterId] != '}')
+                    _characterId ++;
+                _characterId += 2;
+                if(_characterId > _newMsg.length)
+                    _characterId = to!int(_newMsg.length);
+            }
+            if(_characterId == 0)
+                _msg.text = "" ~ _newMsg[0];
+            else
+                _msg.text = _newMsg[0.. _characterId];
+            _characterId ++;
+            _timer2.start(0.02f);
+        }
+    }
+
+    override void draw() {
+        _bubble.draw(center);
     }
 }
