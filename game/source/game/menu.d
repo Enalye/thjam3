e9af60987d78962c7453c95c672eb41ec03203a2
Unknown <enalyen@gmail.com>
2019-01-13 01:56:40 +0100

    Hihg qaulity menu

--- game/source/game/menu.d
+++ game/source/game/menu.d
@@ -24,27 +24,74 @@ void onMainMenu() {
 	addRootGui(new MainMenuGui);
 }
 
+private final class ArrowButton: Button {
+    private {
+        Sprite _sprite;
+    }
+
+    this(string name) {
+        _sprite = fetch!Sprite(name);
+        size(_sprite.size);
+    }
+
+    override void draw() {
+        _sprite.draw(center);
+    }
+}
+
 /// Character selection screen
 final class MainMenuGui: GuiElement {
     string[] files;
+    Sprite[] _sprites;
+    int selectId;
+
+    ArrowButton left, right;
 
     this() {
         size(screenSize);
 
         auto label = new Label("THJAM3");
-        label.setAlign(GuiAlignX.Center, GuiAlignY.Center);
+        label.setAlign(GuiAlignX.Center, GuiAlignY.Top);
         addChildGui(label);
 
         foreach(file; dirEntries("data/doctors/", "{*.json}", SpanMode.depth)) {
             files ~= file;
 
-            //auto json = 
+            auto json = parseJSON(readText(file));
+            _sprites ~= fetch!Sprite(getJsonStr(json, "select"));
+        }
+        
+        left = new ArrowButton("menu_left");
+        left.setAlign(GuiAlignX.Left, GuiAlignY.Center);
+        left.setCallback(this, "left");
+        addChildGui(left);
+
+        right = new ArrowButton("menu_right");
+        right.setAlign(GuiAlignX.Right, GuiAlignY.Center);
+        right.setCallback(this, "right");
+        addChildGui(right);
+    }
+
+    override void onCallback(string id) {
+        switch(id) {
+        case "left":
+            selectId --;
+            if(selectId < 0)
+                selectId = to!int(files.length) - 1;
+            break;
+        case "right":
+            selectId ++;
+            if(selectId >= to!int(files.length))
+                selectId = 0;
+            break;
+        default:
+            break;
         }
     }
 
     override void onSubmit() {
         removeRootGuis();
-        addRootGui(new SceneGui("data/doctors/eirin.json"));
+        addRootGui(new SceneGui(files[selectId]));
     }
 
     override void update(float deltaTime) {
@@ -52,6 +99,6 @@ final class MainMenuGui: GuiElement {
     }
 
     override void draw() {
-
+        _sprites[selectId].draw(center);
     }
 }
